67de86b98bc1cfa891fae89c610b8edffa8dbdc8
Sreekar <sreekar5459@gmail.com>
2023-10-12 15:32:15 -0500

    Integers code change

---
+++ Integers/src/Range.java
@@ -0,0 +1,54 @@
+
+public class Range
+{
+	public static void main(String [] args)
+	{
+		System.out.println("        Checking the Size, Min and Max Values of Numeric DatType       ");
+		System.out.println();
+		
+	        // checking range of byte
+			System.out.println("Size of byte :: "+Byte.SIZE);
+			System.out.println("MinValue of byte :: "+Byte.MIN_VALUE);
+			System.out.println("MaxValue of Byte :: "+Byte.MAX_VALUE);
+			
+			System.out.println();
+			
+			//checking range of short
+			System.out.println("Size of short :: "+Short.SIZE);
+			System.out.println("MinValue of short :: "+Short.MIN_VALUE);
+			System.out.println("MaxValue of Short :: "+Short.MAX_VALUE);
+			
+	         System.out.println();
+			
+			//checking range of integer
+			System.out.println("Size of Integer :: "+Integer.SIZE);
+			System.out.println("MinValue of Integer :: "+Integer.MIN_VALUE);
+			System.out.println("MaxValue of Integer :: "+Integer.MAX_VALUE);
+
+	          System.out.println();
+			
+			//checking range of long
+			System.out.println("Size of long :: "+Long.SIZE);
+			System.out.println("MinValue of long :: "+Long.MIN_VALUE);
+			System.out.println("MaxValue of long :: "+Long.MAX_VALUE);
+			
+	          System.out.println();
+			
+			//checking range of real-number(Float)
+			System.out.println("Size of float :: "+Float.SIZE);
+			System.out.println("MinValue of float :: "+Float.MIN_VALUE);
+			System.out.println("MaxValue of float :: "+Float.MAX_VALUE);
+			
+	          System.out.println();
+			
+			//checking range of real-number(double)
+			System.out.println("Size of Double :: "+Double.SIZE);
+			System.out.println("MinValue of Double :: "+Double.MIN_VALUE);
+			System.out.println("MaxValue of Double :: "+Double.MAX_VALUE);
+			
+		}
+
+	}
+
+
+
