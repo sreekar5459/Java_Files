f1039f617946cd0a9adcca95b0758e4ba299551e
Sreekar <sreekar5459@gmail.com>
2023-10-12 14:42:00 -0500

    Second Commit Changes

---
+++ DataTypes/src/Characters.java
@@ -0,0 +1,21 @@
+
+public class Characters
+{
+   public static void main(String [] args)
+   {
+      double a=21;
+      double b=2;
+      double c=a/b;
+      System.out.println(c);
+      
+      byte num1=21;
+      double num2;
+      num2=num1;
+      System.out.println(num2);
+      
+      double num3=23.4;
+      byte num4;
+      num4=(byte)num3;
+      System.out.println(num4);
+   }
+}
