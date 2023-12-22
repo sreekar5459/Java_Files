f74faa7053c150a425d378817e5a3edc2c334d38
Sreekar <sreekar5459@gmail.com>
2023-12-22 14:03:30 -0600

    FailFast Changes

--- CollectionFramework/src/TestAR1.java
+++ CollectionFramework/src/TestAR1.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class TestAR1 
 {
@@ -14,24 +15,43 @@ public class TestAR1
 		
 		System.out.println(ar);
 		
-		Iterator itr=ar.iterator();
-		while(itr.hasNext())
-		{
-			Integer i=(Integer) itr.next();
-			System.out.println(i);
-			//ar.add(60);//You cannot add the values its result with fail fast or Failsafe and also 
-			//result in
+		
+		                       //Failfast
+		   Iterator itr=ar.iterator();
+		   while(itr.hasNext())
+		    {
+			    //ar.add(60);
+			  System.out.println(itr.next());
+		    }
+			//You cannot add the values  and it results in 
+			
 			//Exception in thread "main" java.util.ConcurrentModificationException
             //at java.base/java.util.ArrayList$Itr.checkForComodification(ArrayList.java:1013)
             //at java.base/java.util.ArrayList$Itr.next(ArrayList.java:967)
             //at TestAR1.main(TestAR1.java:20)
+		   
+		
+		   
+		                      // FailSafe
+		CopyOnWriteArrayList cowal=new CopyOnWriteArrayList();
+		cowal.add(200);
+		cowal.add(300);
+		
+		Iterator itr1=cowal.iterator();
+		while(itr1.hasNext())
+		{
+			
+			System.out.println(itr1.next());
+			cowal.add(123);
 		}
 		
-		ListIterator litr=ar.listIterator(ar.size());
-		while(litr.hasPrevious())
-		{
-			System.out.println(litr.previous());
-		}
+		
+		
+//		ListIterator litr=ar.listIterator(ar.size());
+//		while(litr.hasPrevious())
+//		{
+//			System.out.println(litr.previous());
+//		}
 	}
 
 }
